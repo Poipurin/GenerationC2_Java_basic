066b954d056a96bcdbcbd86f053fd9e03b0c96b9
Poipurin <b.pinorojo@gmail.com>
2022-11-04 01:12:45 -0300

    Java Básico: POO Auto

--- src/cl/generation/f20221027/UsoObjeto.java
+++ src/cl/generation/f20221027/UsoObjeto.java
@@ -3,30 +3,29 @@ package cl.generation.f20221027;
 public class UsoObjeto {
 
 	public static void main(String[] args) {
-		//instancia de clase
-		Auto auto1 = new Auto(); //creación de una variable de tipo Auto//inicializando la instancia// (); ->constructor vacío de auto en este caso
-		//asiganr valores a los atributos privados
-		//solo a través del método setter
+		// instancia de clase
+		Auto auto1 = new Auto(); // creación de una variable de tipo Auto//inicializando la instancia// ();
+									// ->constructor vacío de auto en este caso
+		// asignar valores a los atributos privados
+		// solo a través del método setter
 		auto1.setColor("gris");
 		auto1.setMarca("Mercedes-Benz");
 		auto1.setModelo("Clase G");
-		
-		//consultar el cotenido de los atributos
-		
-		
+
+		// consultar el cotenido de los atributos
+
 		System.out.println(auto1.getColor());
 		System.out.println(auto1.getCilindrada());
-		
-		//saber el contenido de los atributos
+
+		// saber el contenido de los atributos
 		System.out.println(auto1.toString());
-		
-		//nueva instancia de auto
-		Auto car = new Auto("Jeep", "Gris carbón", "Rubicon", 1.6f, 10.0f, 240f );
+
+		// nueva instancia de auto
+		Auto car = new Auto("Jeep", "Gris carbón", "Rubicon", 1.6f, 10.0f, 240f);
 		System.out.println(car.getModelo());
-		
-		
+
 		System.out.println(car.toString());
-		
+
 	}
 
 }
