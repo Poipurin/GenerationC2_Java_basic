387f31fe47392b50bd8daa2543cc758fdb6278ec
Poipurin <b.pinorojo@gmail.com>
2022-11-04 00:25:14 -0300

    Java Básico: Ejercicio calculadora

--- src/cl/generation/f20221027/Menu.java
+++ src/cl/generation/f20221027/Menu.java
@@ -6,15 +6,38 @@ public class Menu {
 
 	public static void main(String[] args) {
 
+		// Menú de opciones
+		// terminar la ejecución		
+		// Calculadora con 4 operaciones básicas
+		// solicitar y capturar 2 números float
+		// imprimir resultados y guardar en una variable
+		// Ofrecer opciones
+		// Definir los métodos
+		// validaciones
+		// limpiar las variables
+		
+		menu();
+
+	}
+
+	public static void menu() {
+		System.out.println("**********************");
+		System.out.println("*   Menú             *");
+		System.out.println("**********************");
+		System.out.println("*1  suma             *");
+		System.out.println("*                    *");
+		System.out.println("*2  resta            *");
+		System.out.println("*                    *");
+		System.out.println("*3  multiplicación   *");
+		System.out.println("*                    *");
+		System.out.println("*4  división         *");
+		System.out.println("*                    *");
+		System.out.println("*0  Salir            *");
+		System.out.println("**********************");
+		
 		Scanner sc = new Scanner(System.in);
 		int opciones = 0; // variables en el bloque superior
 		int contadorErrores=4;
-		
-		
-		// Menú de opsciones
-		// terminar la ejecución
-		menu();
-
 		do {
 			System.out.println("*Seleccione una opción del menú*");
 			opciones = sc.nextInt();
@@ -43,33 +66,47 @@ public class Menu {
 			System.out.println("fuera del while " + opciones);
 		}
 		
+		System.out.println("Ingresa el primer numero");
+		float numero1 = sc.nextFloat();
+		System.out.println("Ingresa el segundo numero");
+		float numero2 = sc.nextFloat();	
+		
+		float resultado = 0;
+		
+		switch (opciones) {
+		case 1:
+			resultado = numero1 + numero2;
+			System.out.println("El resultado es " +resultado);
+			break;
+			
+		case 2:
+			resultado = numero1 - numero2;
+			System.out.println("El resultado es " +resultado);
+			break;
+			
+		case 3:
+			resultado = numero1 * numero2;
+			System.out.println("El resultado es " +resultado);
+			break;
+		case 4:
+			if(numero2 == 0) {
+				System.out.println("No dividas por cero de nuevo o el mundo va a explotar :c");
+			}
+			else {
+				resultado = numero1 / numero2;
+				System.out.println("El resultado es " +resultado);	
+			}
+			break;
+			
+
+		default:
+			System.out.println("Opcion no válida");
+			break;
+		}
 		
 		sc.close();
-		// Calculadora con 4 operaciones básicas
-		// solicitar y capturar 2 números float
-		// imprimir resultados y guardar en una variable
-		// Ofrecer opciones
-		// Definir los métodos
-		// validaciones
-
-		// limpiar las variables
-
-	}
-
-	public static void menu() {
-		System.out.println("**********************");
-		System.out.println("*   Menú             *");
-		System.out.println("**********************");
-		System.out.println("*1  suma             *");
-		System.out.println("*                    *");
-		System.out.println("*2  resta            *");
-		System.out.println("*                    *");
-		System.out.println("*3  multiplicación   *");
-		System.out.println("*                    *");
-		System.out.println("*4  división         *");
-		System.out.println("*                    *");
-		System.out.println("*0  Salir            *");
-		System.out.println("**********************");
+		
+		
 	}
 
 }
