bd511a7ca92eee3aebc3f0c5c326f5e766617e0f
Poipurin <b.pinorojo@gmail.com>
2022-11-04 00:38:47 -0300

    Java Básico: Bucle do while

--- src/cl/generation/f20221026/BucleDoWhileTest.java
+++ src/cl/generation/f20221026/BucleDoWhileTest.java
@@ -8,48 +8,35 @@ public class BucleDoWhileTest {
 		// doWhile
 		// a lo menos se ejecuta una vez
 		Scanner sc = new Scanner(System.in);
-		int edad =0;
-		Float kilos= 0f;
+		int edad = 0;
+		Float kilos = 0f;
 		Float estatura = 0f;
-		
+
 		do {
 			System.out.println("Ingresa tu edad");
 			edad = sc.nextInt();
-			
-			
-		} while (edad < 18 || edad>120);
-		
-		System.out.println("edad ingresada "+edad);
-		
-		
+
+		} while (edad < 18 || edad > 120);
+
+		System.out.println("edad ingresada " + edad);
+
 		do {
 			System.out.println("Ingresa tu peso");
 			kilos = sc.nextFloat();
-			
-			
+
 		} while (kilos < 35 || kilos > 130);
-		
-		System.out.println("peso ingresado "+kilos+"kg");
-		
+
+		System.out.println("peso ingresado " + kilos + "kg");
+
 		do {
 			System.out.println("Ingresa tu estatura");
 			estatura = sc.nextFloat();
-			
-			
+
 		} while (estatura < 1.50 || estatura > 2.65);
-		
-		System.out.println("estatura ingresada "+estatura+"m");
-		
-		
-		
-		
+
+		System.out.println("estatura ingresada " + estatura + "m");
+
 		sc.close();
 	}
-	
-	
-	
-	
-	
-	
-	
+
 }
