64f5d168473c0fc2056f304006f1454f17999901
Poipurin <b.pinorojo@gmail.com>
2022-11-06 21:59:01 -0300

    Java Básico: Ejercicio Lista de estudiantes

--- src/cl/generation/f20221028/Estudiante.java
+++ src/cl/generation/f20221028/Estudiante.java
@@ -1,8 +1,6 @@
 package cl.generation.f20221028;
 
 public class Estudiante {
-
-	Estudiante estudiante = new Estudiante();
 	private String nombre;
 	private String apellido;
 	private Integer edad;
@@ -12,23 +10,20 @@ public class Estudiante {
 	private String correo;
 	
 	
-	
-	//constructor
 	public Estudiante() {
 		super();
 	}
 	
-	   //setter and getters
-
-
-	public Estudiante getEstudiante() {
-		return estudiante;
-	}
-
-
-
-	public void setEstudiante(Estudiante estudiante) {
-		this.estudiante = estudiante;
+	public Estudiante(String nombre, String apellido, Integer edad, String rut, String curso,
+			Integer telefono, String correo) {
+		super();
+		this.nombre = nombre;
+		this.apellido = apellido;
+		this.edad = edad;
+		this.rut = rut;
+		this.curso = curso;
+		this.telefono = telefono;
+		this.correo = correo;
 	}
 
 
@@ -37,86 +32,57 @@ public class Estudiante {
 		return nombre;
 	}
 
-
-
 	public void setNombre(String nombre) {
 		this.nombre = nombre;
 	}
-
-
 
 	public String getApellido() {
 		return apellido;
 	}
 
-
-
 	public void setApellido(String apellido) {
 		this.apellido = apellido;
 	}
-
-
 
 	public Integer getEdad() {
 		return edad;
 	}
 
-
-
 	public void setEdad(Integer edad) {
 		this.edad = edad;
 	}
-
-
 
 	public String getRut() {
 		return rut;
 	}
 
-
-
 	public void setRut(String rut) {
 		this.rut = rut;
 	}
-	
+
 	public String getCurso() {
 		return curso;
 	}
 
-
-
 	public void setCurso(String curso) {
-		this.rut = curso;
+		this.curso = curso;
 	}
-
-
 
 	public Integer getTelefono() {
 		return telefono;
 	}
 
-
-
 	public void setTelefono(Integer telefono) {
 		this.telefono = telefono;
 	}
-
-
 
 	public String getCorreo() {
 		return correo;
 	}
 
-
-
 	public void setCorreo(String correo) {
 		this.correo = correo;
 	}
-
-
-
-	
-	
 	
 	
 }
